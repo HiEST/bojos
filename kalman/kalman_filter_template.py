d11ff88a26ad7863b8e460255123b2a8f57487d3
Alberto Gutiérrez Torre <alberto.gutierrez@bsc.es>
2019-11-16 09:41:49 +0100

    Added kalman

---
+++ kalman/kalman_filter_template.py
@@ -0,0 +1,27 @@
+# +
+import numpy as np
+
+class KalmanFilter(object):
+    def __init__(self, F = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
+
+        if(F is None or H is None):
+            raise ValueError("Set proper system dynamics.")
+
+        self.n = F.shape[1]
+        self.m = H.shape[1]
+
+        self.F = F
+        self.H = H
+        self.B = 0 if B is None else B
+        self.Q = np.eye(self.n) if Q is None else Q
+        self.R = np.eye(self.n) if R is None else R
+        self.P = np.eye(self.n) if P is None else P
+        self.x = np.zeros((self.n, 1)) if x0 is None else x0
+
+    def predict(self, u = 0):
+        # np.dot multiplies matrices. self.F.T will give us the transposed F
+        return 0
+
+    def update(self, z):
+        # np.linalg.inv(S) is the inverse of S
+        I = np.eye(self.n) # Identity
